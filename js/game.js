f21079fd59722fd99394bfdc34ab6be9aa281ba7
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-10-04 17:29:47 -0400

    added 4 new plate divs. top row movement added

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 function movePlate(){
   var $plate = $('.plateTop1');
-  var distance = 1200;
+  var distance = 1280;
   setInterval(function(){
     $plate.css('left', distance + 'px');
     if (distance < -300) {
-      distance = 1200;
+      distance = 1000;
     } else {
       distance -= 5;
     }
@@ -17,11 +17,11 @@ $(document).ready(function(){
 
 function movePlate2(){
   var $plate = $('.plateTop2');
-  var distance = 900;
+  var distance = 1024;
   setInterval(function(){
     $plate.css('left', distance + 'px');
     if (distance < -300) {
-      distance = 1200;
+      distance = 1000;
     } else {
       distance -= 5;
     }
@@ -34,11 +34,11 @@ $(document).ready(function(){
 
 function movePlate3(){
   var $plate = $('.plateTop3');
-  var distance = 600;
+  var distance = 768;
   setInterval(function(){
     $plate.css('left', distance + 'px');
     if (distance < -300) {
-      distance = 1200;
+      distance = 1000;
     } else {
       distance -= 5;
     }
@@ -51,11 +51,11 @@ $(document).ready(function(){
 
 function movePlate4(){
   var $plate = $('.plateTop4');
-  var distance = 300;
+  var distance = 512;
   setInterval(function(){
     $plate.css('left', distance + 'px');
     if (distance < -300) {
-      distance = 1200;
+      distance = 1000;
     } else {
       distance -= 5;
     }
@@ -64,4 +64,21 @@ function movePlate4(){
 
 $(document).ready(function(){
   movePlate4();
+})
+
+function movePlate5(){
+  var $plate = $('.plateTop5');
+  var distance = 256;
+  setInterval(function(){
+    $plate.css('left', distance + 'px');
+    if (distance < -300) {
+      distance = 1000;
+    } else {
+      distance -= 5;
+    }
+  }, 100);
+};
+
+$(document).ready(function(){
+  movePlate5();
 })
