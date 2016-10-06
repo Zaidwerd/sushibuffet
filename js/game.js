4caa68ff4a0786817a38ca87f50c321c0fa078e4
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-10-06 11:27:24 -0400

    add js for bottom plate motion right

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
-function movePlate(){
+// Top Belt
+function movePlateTop(){
   var $plate = $('.plateTop1');
   var distance = 1280;
   setInterval(function(){
@@ -11,11 +12,7 @@ function movePlate(){
   }, 100);
 };
 
-$(document).ready(function(){
-  movePlate();
-})
-
-function movePlate2(){
+function movePlateTop2(){
   var $plate = $('.plateTop2');
   var distance = 1024;
   setInterval(function(){
@@ -28,11 +25,7 @@ function movePlate2(){
   }, 100);
 };
 
-$(document).ready(function(){
-  movePlate2();
-})
-
-function movePlate3(){
+function movePlateTop3(){
   var $plate = $('.plateTop3');
   var distance = 768;
   setInterval(function(){
@@ -45,11 +38,7 @@ function movePlate3(){
   }, 100);
 };
 
-$(document).ready(function(){
-  movePlate3();
-})
-
-function movePlate4(){
+function movePlateTop4(){
   var $plate = $('.plateTop4');
   var distance = 512;
   setInterval(function(){
@@ -62,11 +51,7 @@ function movePlate4(){
   }, 100);
 };
 
-$(document).ready(function(){
-  movePlate4();
-})
-
-function movePlate5(){
+function movePlateTop5(){
   var $plate = $('.plateTop5');
   var distance = 256;
   setInterval(function(){
@@ -80,5 +65,90 @@ function movePlate5(){
 };
 
 $(document).ready(function(){
-  movePlate5();
-})
+  movePlateTop();})
+$(document).ready(function(){
+  movePlateTop2();})
+$(document).ready(function(){
+  movePlateTop3();})
+$(document).ready(function(){
+  movePlateTop4();})
+$(document).ready(function(){
+  movePlateTop5();})
+
+// Bottom Belt
+
+function movePlateBottom(){
+  var $plate = $('.plateBottom5');
+  var distance = 1280;
+  setInterval(function(){
+    $plate.css('right', distance + 'px');
+    if (distance < -300) {
+      distance = 1000;
+    } else {
+      distance -= 5;
+    }
+  }, 100);
+};
+
+function movePlateBottom2(){
+  var $plate = $('.plateBottom4');
+  var distance = 1024;
+  setInterval(function(){
+    $plate.css('right', distance + 'px');
+    if (distance < -300) {
+      distance = 1000;
+    } else {
+      distance -= 5;
+    }
+  }, 100);
+};
+
+function movePlateBottom3(){
+  var $plate = $('.plateBottom3');
+  var distance = 768;
+  setInterval(function(){
+    $plate.css('right', distance + 'px');
+    if (distance < -300) {
+      distance = 1000;
+    } else {
+      distance -= 5;
+    }
+  }, 100);
+};
+
+function movePlateBottom4(){
+  var $plate = $('.plateBottom2');
+  var distance = 512;
+  setInterval(function(){
+    $plate.css('right', distance + 'px');
+    if (distance < -300) {
+      distance = 1000;
+    } else {
+      distance -= 5;
+    }
+  }, 100);
+};
+
+function movePlateBottom5(){
+  var $plate = $('.plateBottom1');
+  var distance = 256;
+  setInterval(function(){
+    $plate.css('right', distance + 'px');
+    if (distance < -300) {
+      distance = 1000;
+    } else {
+      distance -= 5;
+    }
+  }, 100);
+};
+
+$(document).ready(function(){
+  movePlateBottom();})
+$(document).ready(function(){
+  movePlateBottom2();})
+$(document).ready(function(){
+  movePlateBottom3();})
+$(document).ready(function(){
+  movePlateBottom4();})
+$(document).ready(function(){
+  movePlateBottom5();})
