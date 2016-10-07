60d029f0da2db46f3a1da6072b0005fb72a2eec6
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-10-07 12:19:53 -0400

    add emptyplatearr function to stop overlap

--- js/game.js
+++ js/game.js
@@ -1,182 +1,183 @@
 $(document).ready(function (){
 
-  // // Top Belt
-  // function movePlateTop(){
-  //   var $plate = $('#plateTop1');
-  //   var distance = 1000;
-  //   setInterval(function(){
-  //     $plate.css('left', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   },100);
-  // };
-  //
-  // function movePlateTop2(){
-  //   var $plate = $('#plateTop2');
-  //   var distance = 750;
-  //   setInterval(function(){
-  //     $plate.css('left', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // function movePlateTop3(){
-  //   var $plate = $('#plateTop3');
-  //   var distance = 500;
-  //   setInterval(function(){
-  //     $plate.css('left', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // function movePlateTop4(){
-  //   var $plate = $('#plateTop4');
-  //   var distance = 250;
-  //   setInterval(function(){
-  //     $plate.css('left', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // function movePlateTop5(){
-  //   var $plate = $('#plateTop5');
-  //   var distance = 0;
-  //   setInterval(function(){
-  //     $plate.css('left', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // $(document).ready(function(){
-  //   movePlateTop();})
-  // $(document).ready(function(){
-  //   movePlateTop2();})
-  // $(document).ready(function(){
-  //   movePlateTop3();})
-  // $(document).ready(function(){
-  //   movePlateTop4();})
-  // $(document).ready(function(){
-  //   movePlateTop5();})
-  //
-  // // Bottom Belt
-  //
-  // function movePlateBottom(){
-  //   var $plate = $('#plateBottom5');
-  //   var distance = 1000;
-  //   setInterval(function(){
-  //     $plate.css('right', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // function movePlateBottom2(){
-  //   var $plate = $('#plateBottom4');
-  //   var distance = 750;
-  //   setInterval(function(){
-  //     $plate.css('right', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // function movePlateBottom3(){
-  //   var $plate = $('#plateBottom3');
-  //   var distance = 500;
-  //   setInterval(function(){
-  //     $plate.css('right', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // function movePlateBottom4(){
-  //   var $plate = $('#plateBottom2');
-  //   var distance = 250;
-  //   setInterval(function(){
-  //     $plate.css('right', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // function movePlateBottom5(){
-  //   var $plate = $('#plateBottom1');
-  //   var distance = 0;
-  //   setInterval(function(){
-  //     $plate.css('right', distance + 'px');
-  //     if (distance < -300) {
-  //       distance = 1000;
-  //     } else {
-  //       distance -= 5;
-  //     }
-  //   }, 100);
-  // };
-  //
-  // $(document).ready(function(){
-  //   movePlateBottom();})
-  // $(document).ready(function(){
-  //   movePlateBottom2();})
-  // $(document).ready(function(){
-  //   movePlateBottom3();})
-  // $(document).ready(function(){
-  //   movePlateBottom4();})
-  // $(document).ready(function(){
-  //   movePlateBottom5();})
+  // Top Belt
+function movePlateTop(){
+    var $plate = $('#plateTop1');
+    var distance = 1000;
+    setInterval(function(){
+      $plate.css('left', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    },100);
+  };
+
+function movePlateTop2(){
+    var $plate = $('#plateTop2');
+    var distance = 750;
+    setInterval(function(){
+      $plate.css('left', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+function movePlateTop3(){
+    var $plate = $('#plateTop3');
+    var distance = 500;
+    setInterval(function(){
+      $plate.css('left', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+function movePlateTop4(){
+    var $plate = $('#plateTop4');
+    var distance = 250;
+    setInterval(function(){
+      $plate.css('left', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+function movePlateTop5(){
+    var $plate = $('#plateTop5');
+    var distance = 0;
+    setInterval(function(){
+      $plate.css('left', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+movePlateTop();
+movePlateTop2();
+movePlateTop3();
+movePlateTop4();
+movePlateTop5();
+
+  // Bottom Belt
+
+function movePlateBottom(){
+    var $plate = $('#plateBottom5');
+    var distance = 1000;
+    setInterval(function(){
+      $plate.css('right', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+function movePlateBottom2(){
+    var $plate = $('#plateBottom4');
+    var distance = 750;
+    setInterval(function(){
+      $plate.css('right', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+function movePlateBottom3(){
+    var $plate = $('#plateBottom3');
+    var distance = 500;
+    setInterval(function(){
+      $plate.css('right', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+function movePlateBottom4(){
+    var $plate = $('#plateBottom2');
+    var distance = 250;
+    setInterval(function(){
+      $plate.css('right', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+function movePlateBottom5(){
+    var $plate = $('#plateBottom1');
+    var distance = 0;
+    setInterval(function(){
+      $plate.css('right', distance + 'px');
+      if (distance < -300) {
+        distance = 1000;
+      } else {
+        distance -= 5;
+      }
+    }, 100);
+  };
+
+movePlateBottom();
+movePlateBottom2();
+movePlateBottom3();
+movePlateBottom4();
+movePlateBottom5();
 
 var plateArr = $('.sushiPlate');
+var emptyPlates = [];
+
+function updateEmptyPlateArr(){
+  for(var i = 0; i < plateArr.length; i++){
+    if(!plateArr[i].html()){
+      emptyPlates.push(plateArr[i])
+      console.log('empty')
+    }
+  }
+};
 
 function randomPlate(array) {
-   return array[Math.floor(Math.random() * array.length)];
+   return array[Math.floor(Math.random() * plateArr.length)];
 }
 
 function addGoodSushi(){
-  if ($('.sushiPlate:empty')){
-    $(randomPlate(plateArr)).append("<img src='images/goodSushi.png' class= 'goodSushi'/>")
-  }};
+    console.log('makin good sush')
+    $(randomPlate(emptyPlates)).append("<img src='images/goodSushi.png' class= 'goodSushi'/>")
+};
 
 function addAmazingSushi(){
-  $(randomPlate(plateArr)).append("<img src='images/amazingSushi.png' class= 'amazingSushi'/>")
+    console.log('makin some other good sush')
+    $(randomPlate(emptyPlates)).append("<img src='images/amazingSushi.png' class= 'amazingSushi'/>");
 };
 
 
 function populateGoodSushi(){
     addGoodSushi();
     addAmazingSushi();
-    setInterval(populateGoodSushi, 3000);
 }
 
+setInterval(populateGoodSushi, 3000);
 setTimeout(populateGoodSushi, 5000);
 
 $('.goodSushi').click(function() {
